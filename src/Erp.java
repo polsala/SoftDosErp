385a9a2c9414b7b8ccc208ada9a722307440dd40
Exor_P <exor_p@exor.p>
2017-12-23 12:24:30 +0100

    PAram -g to call gui in params main.

--- src/Erp.java
+++ src/Erp.java
@@ -10,8 +10,18 @@ public class Erp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        boolean gui_on = false;
+        for (String arg : args) {
+            if (arg.equals("-g")) {
+                gui_on = true;
+            }
+            System.out.println(arg);
+        }
+        if (gui_on){
+            main_menu.main(args);
+        }
         // TODO code application logic here
-        main_menu.main(args);
+        
     }
     
 }
