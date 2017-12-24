76ffaac0bd1151dd4f43230dcc16e21eecccdf12
polsala <polsalalidon@gmail.com>
2017-12-24 11:06:09 +0100

    Added create_table and search_table for DB.

--- src/date_base/DateBase.java
+++ src/date_base/DateBase.java
@@ -3,7 +3,30 @@
  */
 package date_base;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 public class DateBase {
-    
+     private Map<Object,Map<Object,Object>> mapOfMaps;
+     
+     public DateBase(){
+         mapOfMaps = new LinkedHashMap<Object,Map<Object,Object>>();
+     }
+     
+     public boolean create_table(String table_name){
+         // if key doesn't exist create table and return True else return False 
+         Object table_key = new String(table_name);
+         Object res = mapOfMaps.putIfAbsent(table_key, new LinkedHashMap<Object,Object>());
+         return res == null;
+     }
+     
+     public Map<Object,Object> search_table(String table_name){
+         // Return value of mapOfMaps with key if exist, else return null;
+         Object table_key = new String(table_name);
+         Map<Object,Object> value = mapOfMaps.get(table_key);
+         return value;
+     }
+     
+     
 }
