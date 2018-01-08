b565a14574abc982f4371d6e657aae9b947edda7
polsala <polsalalidon@gmail.com>
2018-01-08 18:45:30 +0100

    Init class CLient.

---
+++ src/date_base/Client.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package date_base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author exor_p
+ */
+public class Client {
+    public long _id;
+    private String _name;
+    private String _dni;
+    private String _direccio;
+    private long _telefon;
+    private long _tarifa_id;
+    int _punts;
+    
+    public Client(){
+        
+    }
+}
