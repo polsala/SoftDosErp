c92fa43135c328870409a2d9c13d6dcc53b750b5
polsala <polsalalidon@gmail.com>
2018-01-08 19:55:21 +0100

    Add constraints on create client.

--- src/date_base/Client.java
+++ src/date_base/Client.java
@@ -7,6 +7,7 @@ package date_base;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -17,11 +18,24 @@ public class Client {
     private String _name;
     private String _dni;
     private String _direccio;
-    private long _telefon;
-    private long _tarifa_id;
+    private Long _telefon;
+    private Long _tarifa_id;
     int _punts;
     
-    public Client(){
+    public Client(Long id, String name, String dni, String direccio, Long tel, Long tarifaid) throws Exception{
         
+        if(id == null || id < 1 || name.equals("") || dni.equals("")  ){
+            throw new Exception("ERROR on Creating Client id, name and dni "
+                    + "requiered\n");
+        }
+        else{
+            _id = id;
+            _name = name;
+            _dni = dni;
+            _direccio = direccio;
+            _telefon = tel;
+            _tarifa_id = tarifaid;
+            _punts = 0;
+        }
     }
 }
