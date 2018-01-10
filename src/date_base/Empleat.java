1e30e453014c1bcb257c883856c646633fb420c2
polsala <polsalalidon@gmail.com>
2018-01-10 21:03:12 +0100

    Empleat class init.

---
+++ src/date_base/Empleat.java
@@ -0,0 +1,13 @@
+package date_base;
+
+
+public class Empleat extends Generic{
+    Long _id;
+    String _name;
+    String _dni;
+    String _compte_banc;
+    String _direccio;
+    Long _telefon;
+    Integer _contract_type; 
+    
+}
