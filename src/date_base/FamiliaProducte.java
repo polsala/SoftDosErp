cc3e8d865b4ca1de5333ba1d4c5c0cbb3df5bcbb
polsala <polsalalidon@gmail.com>
2018-01-11 18:10:19 +0100

    Rm headers.

--- src/date_base/FamiliaProducte.java
+++ src/date_base/FamiliaProducte.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package date_base;
 
-/**
- *
- * @author exor_p
- */
 public class FamiliaProducte extends Generic{
     public Long _id;
     public String _name;
--- src/date_base/LiniaFactura.java
+++ src/date_base/LiniaFactura.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package date_base;
 
-/**
- *
- * @author exor_p
- */
 public class LiniaFactura {
     public Integer _num;
     public Producte _producte;
