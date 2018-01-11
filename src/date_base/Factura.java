486ea6a5707fa7ff46f0534b40b891ca88a9dfa5
polsala <polsalalidon@gmail.com>
2018-01-11 19:07:36 +0100

    Added client on factura.

--- src/date_base/Factura.java
+++ src/date_base/Factura.java
@@ -9,9 +9,10 @@ public class Factura extends Generic{
     public Float _total_import;
     public Integer _num_caixa;
     public Long _empleat_id;
+    public Long _client_id;
     public LocalDateTime _data_facturacio;
     
-    public Factura(Long id, String name, Integer num, Long e_id) throws Exception{
+    public Factura(Long id, String name, Integer num, Long e_id, Long cli) throws Exception{
         if (id == null || name.equals("") || num == null || e_id == null){
             throw new Exception("Error on create Factura id, import total, "
                     + "numero de caixa and employer id are requiered");
@@ -20,6 +21,7 @@ public class Factura extends Generic{
             _name = name;
             _num_caixa = num;
             _empleat_id = e_id;
+            _client_id = cli;
             _data_facturacio = LocalDateTime.now();
             System.out.println(_data_facturacio);
         }
