e473d090f5ea527b0f9cc589c6770155b0b23b38
polsala <polsalalidon@gmail.com>
2018-01-11 18:00:39 +0100

    FamiliaProducte class init.

---
+++ src/date_base/FamiliaProducte.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package date_base;
+
+/**
+ *
+ * @author exor_p
+ */
+public class FamiliaProducte extends Generic{
+    public Long _id;
+    public String _name;
+    public String _info;
+    public Float _impost_afegit;
+    
+    
+}
