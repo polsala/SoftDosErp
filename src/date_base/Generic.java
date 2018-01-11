9ffd1280b7a02c4d0a4cdc0cb514f7343eb3fdf0
polsala <polsalalidon@gmail.com>
2018-01-11 17:38:36 +0100

    New generic method to set fields of Generic inherit

--- src/date_base/Generic.java
+++ src/date_base/Generic.java
@@ -15,4 +15,18 @@ public class Generic {
         return null;
     }
     
+    public Boolean write_att(String att_name, Object value) throws NoSuchFieldException{
+        try{
+            Field f = this.getClass().getField(att_name);
+            f.set(this, value);
+            //f.setAccessible(true);
+            System.out.println("Valor sobrescrit");
+            return true;
+        }catch(Exception e){
+            System.err.println("Error no s'ha pogut sobreescriure");
+            System.out.println(e);
+        }
+        return false;
+    }
+    
 }
