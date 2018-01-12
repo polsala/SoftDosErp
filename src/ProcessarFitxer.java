47779691b7f5003d44f585daf63b079ed1ad4b53
polsala <polsalalidon@gmail.com>
2018-01-12 12:41:56 +0100

    Process file class.

---
+++ src/ProcessarFitxer.java
@@ -0,0 +1,10 @@
+import date_base.*;
+import java.io.File;
+
+
+public class ProcessarFitxer {
+    public Boolean procesar_fitxer(DateBase cursor, File file){
+        
+        return true;
+    }
+}
