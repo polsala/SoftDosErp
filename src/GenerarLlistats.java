a7abb6f546fbaf83349a57ce60eed00bd75b1b0b
polsala <polsalalidon@gmail.com>
2018-01-20 22:08:23 +0100

    Improved llistats.

--- src/GenerarLlistats.java
+++ src/GenerarLlistats.java
@@ -7,26 +7,31 @@ import java.util.Set;
 
 public class GenerarLlistats {
     
+    public static String fixedLengthString(String s, int n) {
+        return String.format("%1$-" + n + "s", s);
+        //return String.format("%1$"+length+ "s", string);
+    }
+    
     public static void llistar_invenaris(DateBase cursor){
-        final String header = "    Codi       Article      ";
+        final String header = "    Codi                      Article";
         Map<Object,Object> inventaris = cursor.search_table("Inventari");
         if (inventaris != null){
-            System.out.print(header);
+            System.out.print(fixedLengthString(header,77));
             inventaris.entrySet().forEach((entry) -> {
                     Inventari i = Inventari.class.cast(entry.getValue());
                     String name = i._name;
-                    System.out.print(i + "   ");
+                    System.out.print(fixedLengthString(name,16));
                 });
             System.out.print("\n");
             Set<Object> prod_keys = cursor.get_table_keys("Producte");
             for(Object o : prod_keys){
                 Long id_p = (Long) o;
                 Producte pp = Producte.class.cast(cursor.search_by_id("Producte", id_p));
-                System.out.print(pp._id + "     " + pp._name + "    ");
+                System.out.print(pp._id + "     " + fixedLengthString(pp._name,50) + "    ");
                 inventaris.entrySet().forEach((entry) -> {
                     Inventari i = Inventari.class.cast(entry.getValue());
                     Integer unitats = i.consultar_unitats(id_p);
-                    System.out.print(unitats + "   ____");
+                    System.out.print("      " + unitats + "   ____");
                 });
                 System.out.print("\n");
             }
